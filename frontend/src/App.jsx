349476de5fe733767e5cd31a546817d2221f5bff
Divya R <kwrdivyaraj100@gmail.com>
2023-11-03 11:16:36 +0530

    Importing login. navbar and restaurant components

---
+++ frontend/src/App.jsx
@@ -0,0 +1,20 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import './App.css';
+import Navbar from './component/navbar/Navbar';
+import Login from './component/login/Login';
+import Restaurant from './component/restaurant/Restaurant';
+
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route exact path='/login' element={<Login />}/>
+        <Route exact path='/restaurant' element={<Restaurant />}/>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;

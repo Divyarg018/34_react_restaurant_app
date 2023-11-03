f7ed1a33a99bbdac6c5734e9c84d8495903ed8f7
Divya R <kwrdivyaraj100@gmail.com>
2023-11-03 11:40:54 +0530

    Synthetic events and target outputs are coming as expected

--- frontend/src/component/login/Login.jsx
+++ frontend/src/component/login/Login.jsx
@@ -1,9 +1,23 @@
 import './Login.css';
 
 function Login(props){
+
+    function handleSubmit(e){
+        e.preventDefault();
+        console.log(e.target);
+        console.log(e.target.username);
+        console.log(e.target.username.value);
+        console.log(e.target.password);
+        console.log(e.target.password.value);
+    }
     return(
         <div>
-            <h1>Login Component</h1>
+          <h1>Login Form</h1>
+          <form onSubmit={handleSubmit}>
+            Username: <input type='text' name='username' id='username' /><br />
+            Password: <input type='password' name='password' id='password' />
+            <input type='submit' value="login" />
+          </form>
         </div>
     );
 }

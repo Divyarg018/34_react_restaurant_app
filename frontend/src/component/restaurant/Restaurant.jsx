b53abcd4906a883b993cb5c8adabae46bbc59894
Divya R <kwrdivyaraj100@gmail.com>
2023-11-06 12:20:20 +0530

    React JSX array rendering

--- frontend/src/component/restaurant/Restaurant.jsx
+++ frontend/src/component/restaurant/Restaurant.jsx
@@ -1,31 +1,32 @@
 import { useState, useEffect } from 'react';
 import './Restaurant.css';
 
-function Restaurant(props){
+function Restaurant(props) {
     const [menuData, setMenuData] = useState({
         flag: false,
         foodData: null,
         drinkData: null
     });
 
-    async function getData(){
+    async function getData() {
         let backend_url = 'http://localhost:3200/data';
         let response = await fetch(backend_url);
-let responseData = await response.json();
-      setMenuData = {
-        flag: true,
-        foodData: responseData[0],
-        drinkData: responseData[1]
-      };
+        let responseData = await response.json();
+        // setMenuData = {
+        //     flag: true,
+        //     foodData: responseData[0],
+        //     drinkData: responseData[1]
+        // };
     }
 
-    useEffect(() =>{
+    useEffect(() => {
         getData();
     }, []);
 
-    return(
+    let arr = ['divya', 'archana', 'chaithanya'];
+    return (
         <div>
-            <h1>Restaurant Component</h1>
+            <h1>{ arr }</h1>
         </div>
     );
 }

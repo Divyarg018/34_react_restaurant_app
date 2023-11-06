d7c1d78c5d4c7a23a1d91b588f835361406bd1a4
Divya R <kwrdivyaraj100@gmail.com>
2023-11-06 13:35:33 +0530

    Using card component of bootstrap for food-container

--- frontend/src/component/restaurant/Restaurant.jsx
+++ frontend/src/component/restaurant/Restaurant.jsx
@@ -1,5 +1,7 @@
 import { useState, useEffect } from 'react';
 import './Restaurant.css';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
 
 function Restaurant(props) {
     const [menuData, setMenuData] = useState({
@@ -28,13 +30,26 @@ function Restaurant(props) {
             <div>
                 <h1> Food Menu</h1>
 
+<div className='food-container'>
                 {
                     menuData.flag ? menuData.foodData.map(item => {
-                        return <li key={item.foodName}>
-                            Food Name is {item.foodName}, Price: {item.price}, Category: {item.category}
-                        </li>
+                        return   <Card className= 'food-item' style={{ width: '18rem' }} key={item.foodName}>
+                        <Card.Img variant="top" src={'images/food/' + item.foodName + '.jpg'} />
+                        <Card.Body className={(item.category === 'veg') ? 'veg' : 'non-veg'}>
+                          <Card.Title>{item.foodName} : {item.price} </Card.Title>
+                          <Card.Text>
+                          {item.foodName} is an amazing Dish. You should try it.
+                          </Card.Text>
+                          Category:
+                          <img onScroll={(item.category === 'veg') ? 'images/veg.png' : 'images/non-veg.png'}
+                          height="30px" width="30px" />
+                          <Button variant="primary">Buy Now</Button>
+                        </Card.Body>
+                      </Card>
                     }) : <h1>Loading</h1>
                 }
+
+</div>
 
                 <h1> Drink Menu</h1>
 

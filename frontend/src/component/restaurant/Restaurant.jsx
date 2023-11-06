ed1e5f0e396abe3ff6a3241b370554ced001c531
Divya R <kwrdivyaraj100@gmail.com>
2023-11-06 12:10:52 +0530

    Modifying restaurant.js

--- frontend/src/component/restaurant/Restaurant.jsx
+++ frontend/src/component/restaurant/Restaurant.jsx
@@ -2,7 +2,17 @@ import { useState, useEffect } from 'react';
 import './Restaurant.css';
 
 function Restaurant(props){
-   
+ 
+
+    async function getData(){
+        let backend_url = 'http://localhost:3200/data';
+        let response = await fetch(backend_url);
+
+        let responseData = await response.json();
+        console.log(responseData);
+    }
+
+  
 
     return(
         <div>
